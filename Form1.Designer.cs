aad2668b442d5719ba26fd51393c213684fedae8
sbm_deni <sbm_deni@SBMB-NB-IT0394>
2024-04-17 10:38:26 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ScannerSonar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox0 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox0 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Initialization";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox0
+            // 
+            this.richTextBox0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox0.Location = new System.Drawing.Point(3, 16);
+            this.richTextBox0.Name = "richTextBox0";
+            this.richTextBox0.Size = new System.Drawing.Size(635, 374);
+            this.richTextBox0.TabIndex = 1;
+            this.richTextBox0.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox0);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 152);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(641, 393);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(171, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Build";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(249, 116);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 30);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Scan";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "WRC_SYSTEM_DATACENTER";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Project";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Host URL";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(79, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(242, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "http://192.168.152.129:9000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Key";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(79, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(242, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "sqp_62b74c4ee8505274b008f410fba8f0d701eeca13";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "(?)";
+            this.toolTip1.SetToolTip(this.label4, "eg: WRC_QA");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(327, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "(?)";
+            this.toolTip1.SetToolTip(this.label5, "URL of SonarQube");
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(327, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "(?)";
+            this.toolTip1.SetToolTip(this.label6, "Key for projects");
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(327, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "(?)";
+            this.toolTip1.SetToolTip(this.label7, ".sln root path");
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = ".sln Path";
+            // 
+            // textBox0
+            // 
+            this.textBox0.Location = new System.Drawing.Point(79, 12);
+            this.textBox0.Name = "textBox0";
+            this.textBox0.Size = new System.Drawing.Size(242, 20);
+            this.textBox0.TabIndex = 15;
+            this.textBox0.Text = "C:\\Users\\sbm_deni\\Desktop\\RND STUFF\\STRESS TEST\\WRC_TRUNK";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 545);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox0);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(657, 584);
+            this.MinimumSize = new System.Drawing.Size(657, 584);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Sonar Scanner";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox0;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox0;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ScannerSonar
+{
+    public partial class Form1 : Form
+    {
+        private Process cmdProcess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void AppendTextToRichTextBox(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => AppendTextToRichTextBox(text)));
+            }
+            else
+            {
+                richTextBox0.AppendText(text);
+            }
+        }
+        private void SendCommandToCmd(string command)
+        {
+            try
+            {
+                if (cmdProcess != null && !cmdProcess.HasExited)
+                {
+                    cmdProcess.StandardInput.WriteLine(command);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo cmdStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
+            cmdProcess = new Process
+            {
+                StartInfo = cmdStartInfo,
+                EnableRaisingEvents = true
+            };
+            cmdProcess.OutputDataReceived += (s, args) =>
+            {
+                if (!string.IsNullOrEmpty(args.Data))
+                {
+                    AppendTextToRichTextBox(args.Data + Environment.NewLine);
+                }
+            };
+            cmdProcess.Start();
+            cmdProcess.BeginOutputReadLine();
+
+            
+            string startupFolder = textBox0.Text;
+            string command = $@"cd ""{startupFolder}""";
+            SendCommandToCmd(command);
+        }
+        public void button2_Click(object sender, EventArgs e)
+        {
+            string command = $@"SonarScanner.MSBuild.exe begin /k:""{textBox1.Text}"" /d:sonar.host.url=""{textBox2.Text}"" /d:sonar.login=""{textBox3.Text}""";
+            SendCommandToCmd(command);
+        }
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            string startupFolder = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "MsBuild", "MsBuild.exe");
+            string msBuildPath = startupFolder;
+            string targetDirectory = textBox0.Text;
+            string command = $@"""{msBuildPath}"" /t:Rebuild /p:WorkingDirectory=""{targetDirectory}""";
+            SendCommandToCmd(command);
+
+        }
+
+        public void button4_Click(object sender, EventArgs e)
+        {
+            string command = $@"SonarScanner.MSBuild.exe end /d:sonar.login=""{textBox3.Text}""";
+            SendCommandToCmd(command);
+        }
+    }
+}
